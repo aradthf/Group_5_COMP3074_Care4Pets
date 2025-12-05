356bc559321c03af76d602834db8a3b1e3a92ba3
Najnins <najnin.sultana@georgebrown.ca>
2025-12-04 23:44:24 -0500

    Onboarding UI Updates – Improved Layout, Indicators, fix SKIP option and Validation for login and signup

--- app/src/main/java/com/arad/care4pets/LoginActivity.java
+++ app/src/main/java/com/arad/care4pets/LoginActivity.java
@@ -22,19 +22,31 @@ public class LoginActivity extends AppCompatActivity {
         Button btnCreateAccount = findViewById(R.id.btnCreateAccount);
 
         btnLogin.setOnClickListener(v -> {
-        String email = etEmail.getText() != null ? etEmail.getText().toString().trim() : "";
-        String pass = etPassword.getText() != null ? etPassword.getText().toString().trim() : "";
+            String email = etEmail.getText() != null ? etEmail.getText().toString().trim() : "";
+            String pass = etPassword.getText() != null ? etPassword.getText().toString().trim() : "";
 
-        if (!email.isEmpty() && !pass.isEmpty()) {
+            // Check empty fields
+            if (email.isEmpty() || pass.isEmpty()) {
+                Toast.makeText(LoginActivity.this,
+                        "Please enter email and password",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // EMAIL VALIDATION
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(LoginActivity.this,
+                        "Please enter a valid email address",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // If valid → continue login
             Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
             startActivity(intent);
             finish();
-        } else {
-            Toast.makeText(LoginActivity.this,
-                "Please enter email and password",
-                Toast.LENGTH_SHORT).show();
-        }
-    });
+        });
+
 
         btnCreateAccount.setOnClickListener(v -> {
         Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
